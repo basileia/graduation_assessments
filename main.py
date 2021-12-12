588ff4a89addf68c399c51e504972ddd7b0e641e
basileia <jane.novak@centrum.cz>
2021-12-12 19:32:51 +0100

    initial commit

---
+++ main.py
@@ -0,0 +1,57 @@
+import os
+from flask_wtf import FlaskForm
+from flask import Flask, render_template
+from wtforms import IntegerField, widgets
+
+app = Flask(__name__)
+app.secret_key = os.environ.get('SECRET_KEY', 'dev')
+
+
+class MyForm(FlaskForm):
+    test_points = IntegerField("Písemná práce - počet bodů", widget=widgets.Input(input_type="number"))
+    oral_exam_points = IntegerField("Ústní zkouška - počet bodů", widget=widgets.Input(input_type="number"))
+
+
+def calculate_score(test_points, oral_exam_points):
+    # kritéria pro hodnocení profilové části maturitní zkoušky jazyka anglického
+    # rok 2022
+    min_points_test = 16
+    max_points_test = 36
+    test_coef = 0.4
+    min_points_oral_exam = 18
+    max_point_oral_exam = 39
+    oral_exam_coef = 0.6
+
+    if test_points < min_points_test or oral_exam_points < min_points_oral_exam:
+        return "Nedostatečný - 5"
+    else:
+        test_perc = test_points / (max_points_test / 100) * test_coef
+        oral_exam_perc = oral_exam_points / (max_point_oral_exam / 100) * oral_exam_coef
+        perc = test_perc + oral_exam_perc
+        if 88 <= perc < 100:
+            return "Výborný - 1"
+        elif 73 < perc < 87:
+            return "Chvalitebný - 2"
+        elif 58 < perc <= 73:
+            return "Dobrý - 3"
+        elif 44 <= perc <= 58:
+            return "Dostatečný - 4"
+        elif perc < 44:
+            return "Nedostatečný - 5"
+        else:
+            return "Někde se stala chyba."
+
+
+@app.route("/", methods=["GET", "POST"])
+def show_score():
+    form = MyForm()
+    if form.validate_on_submit():
+        test_points = form.test_points.data
+        oral_exam_points = form.oral_exam_points.data
+        result = calculate_score(test_points, oral_exam_points)
+        return render_template("index.html", result=result, form=form)
+    return render_template("index.html", form=form)
+
+
+if __name__ == "__main__":
+    app.run(debug=False)
